d8382195b6120662da782cafdfa0ee64292246b7
khushikukreti <kukreti.khushi@gmail.com>
2021-09-29 13:50:49 +0530

    zzz

--- pages/TopCollection.js
+++ pages/TopCollection.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 function TopCollection({ pname, imgURL }) {
   return (
     <div style={{padding:"30px"}}>
-    <div class="daily-essential1aa" >
+    <div className="daily-essential1aa" >
       
       <a href="">
         <Image src={`${imgURL}`} alt="	" layout="fill" />
       </a>
       <div className="text">
-        <p class="daily-text">{pname}</p>
+        <p className="daily-text">{pname}</p>
       </div>
       </div>
       </div>
